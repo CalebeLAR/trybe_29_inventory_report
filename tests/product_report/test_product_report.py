f074329eb7fcede502241a8669819c5e4f054ca6
calebeLAR <ca6ele@gmail.com>
2023-09-02 14:22:04 -0400

    req02: implementa teste para o relatório individual gerado por Produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,22 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="id",
+        product_name="farinha",
+        company_name="Farinini",
+        manufacturing_date="01-05-2021",
+        expiration_date="01-01-2022",
+        serial_number="TY68 409C JJ43 ASD1 PL2F",
+        storage_instructions=(
+            "precisa ser armazenado em local protegido da luz"
+        ),
+    )
+
+    assert str(product) == (
+        "The product id - farinha with serial number TY68 409C JJ43 ASD1 PL2F "
+        "manufactured on 01-05-2021 by the company Farinini valid until "
+        "01-01-2022 must be stored according to the following instructions: "
+        "precisa ser armazenado em local protegido da luz."
+    )
