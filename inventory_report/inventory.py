2ea5283365e192ebb61f771e6e2c09ee04012f07
calebeLAR <ca6ele@gmail.com>
2023-09-04 10:51:16 -0400

    req05: implementa classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,17 @@
+# from typing import Type
+# from inventory_report.importers import JsonImporter
+from inventory_report.product import Product
+
+PATH = "inventory_report/data/inventory.json"
+
+
 class Inventory:
-    pass
+    def __init__(self, data: list[Product] | None = None) -> None:
+        self.__data = [] if not data else data
+
+    def add_data(self, data: list[Product]) -> None:
+        self.__data.extend(data)
+
+    @property
+    def data(self) -> list[Product]:
+        return self.__data
